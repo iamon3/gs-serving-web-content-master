f8231672ec02717efee606e498a7138d35b21c06
free coder <iamon3.free@gmail.com>
2019-11-30 14:03:04 +0530

    Added new line watcher thread logi

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -3,8 +3,14 @@ package hello;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.stream.Stream;
 
 import org.springframework.core.io.ClassPathResource;
@@ -31,11 +37,39 @@ public class GreetingController {
 		return "logs";
 	}
 
+	private boolean fileWatcherLogic(String fileName) throws IOException, InterruptedException {
+		
+		Resource resource = new ClassPathResource(fileName);
+		File file = resource.getFile();
+		final Path path = file.toPath(); //FileSystems.getDefault().getPath(System.getProperty("user.home"), "Desktop");
+		
+		System.out.println(path);
+		try (final WatchService watchService = FileSystems.getDefault().newWatchService()) {
+		    final WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+		    while (true) {
+		        final WatchKey wk = watchService.take();
+		        for (WatchEvent<?> event : wk.pollEvents()) {
+		            //we only register "ENTRY_MODIFY" so the context is always a Path.
+		            final Path changed = (Path) event.context();
+		            System.out.println(changed);
+		            if (changed.endsWith(fileName)) {
+		                System.out.println(String.format("%s  Changes", fileName));
+		            }
+		        }
+		        // reset the key
+		        boolean valid = wk.reset();
+		        if (!valid) {
+		            System.out.println("Key has been unregisterede");
+		        }
+		    }
+		}	
+	}
+	
 	private String readFile(String fileName) {
 		StringBuilder sb = new StringBuilder();
 		Resource resource = new ClassPathResource(fileName);
 		try {
-			InputStream input = resource.getInputStream();
+			//InputStream input = resource.getInputStream();
 			File file = resource.getFile();
 			try (Stream<String> stream = Files.lines(file.toPath())){//Paths.get(fileName))) {
 				stream.forEach(sb::append);
